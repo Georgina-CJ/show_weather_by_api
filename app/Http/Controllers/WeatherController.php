af7aaab7efaa7d07a13ff7943de8269ea59e1552
Georgina <forwork0525@gmail.com>
2020-06-11 19:11:31 +0800

    get weather info from api

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+
+class WeatherController extends Controller
+{
+    public function showTable()
+    {
+        if (!Auth::check()) {
+            return redirect('/login');
+        }
+
+        $client = new \GuzzleHttp\Client();
+        $result = $client->request('GET', 'https://tinyurl.com/y7y54gms');
+        $data = json_decode($result->getBody());
+
+        $tittle = $data->records->datasetDescription;
+        $info_list = $data->records->location;
+        $weathers = [];
+        foreach ($info_list as $key => $list) {
+            $weathers[$key]['locationName'] = $list->locationName;
+            $weathers[$key]['startTime'] = $list->weatherElement[0]->time[0]->startTime;
+            $weathers[$key]['endTime'] = $list->weatherElement[0]->time[0]->endTime;
+            $weathers[$key]['status'] = $list->weatherElement[0]->time[0]->parameter->parameterName;
+            $cc_low = $list->weatherElement[2]->time[0]->parameter;
+            $cc_high = $list->weatherElement[4]->time[0]->parameter;
+            $weathers[$key]['temp_low'] = $cc_low->parameterName .'°'.$cc_low->parameterUnit;
+            $weathers[$key]['temp_high'] = $cc_high->parameterName .'°'.$cc_high->parameterUnit;
+            $weathers[$key]['feeling'] = $list->weatherElement[3]->time[0]->parameter->parameterName;
+        }
+        $data=[];
+        $data['tittle'] = $tittle;
+        $data['list'] = $weathers;
+        return view('home', $data);
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">{{ $tittle ?? 'Dashboard' }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,37 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="" name="location"  >地點</th>
+                                <th class="" name="temp"  >期間</th>
+                                <th class="" name="weather"  >天氣</th>
+                                <th class="" name="weather"  >最低溫度</th>
+                                <th class="" name="weather"  >最高溫度</th>
+                                <th class="" name="weather"  >體感</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                    You are logged in!
+                        @if($list)
+                            @foreach ($list as $l)
+                            <tr>
+                                <td class="">{{ $l['locationName'] ?? ''}}</td>
+                                <td class="">{{ $l['startTime'] . '~' .$l['endTime']}}</td>
+                                <td class="">{{ $l['status'] ?? ''}}</td>
+                                <td class="">{{ $l['temp_low'] ?? ''}}</td>
+                                <td class="">{{ $l['temp_high'] ?? ''}}</td>
+                                <td class="">{{ $l['feeling'] ?? ''}}</td>
+                            </tr>
+                            @endforeach
+                        @endif
+                        </tbody>
+                    </table>
+
+
+
+
                 </div>
             </div>
         </div>
